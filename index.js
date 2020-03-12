85f556906aea05e2d29b7f6cecba514327421ac8
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-12 15:43:45 +0000

    latest test

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const path = require('path')
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+const PORT = process.env.PORT || 9000
+
+app.get('/api', (req,res) => {
+    res.json({ message: 'The api is UP' })
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
